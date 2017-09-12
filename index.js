ca8f37904908b427cdc5414361101edd8f43aa1b
Mike Fleming <flemingatx@gmail.com>
2017-09-12 08:11:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+module.exports = (options) => {
+  const app = options.app;
+  // const passport = options.passport;
+  const db = options.db;
+
+  mongoose.connect(db);
+
+  // configure passport before anything else
+  require('./config/passport')(passport);
+
+  app.use(cookieParser());
+  app.use(bodyParser());
+
+  app.set('view engine', 'ejs');
+
+  app.use(session({ secret: 'olafishtaroctavia' })); // make this env
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(flash());
+
+  require('./app/routes')(app, passport);
+};
